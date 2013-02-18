7fd7210b5140c94cdfe893693a86612fe8e80cf7
tadamak <tadamak@gmail.com>
2013-02-18 16:51:14 +0900

    zsh: change prompt face

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -52,7 +52,7 @@ rb_prompt() {
 #http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
 hg_prompt() {
   hg prompt --angle-brackets "\
-<on %{$fg[magenta]%}<branch>%{$reset_color%}>\
+<on %{$fg_bold[red]%}<branch>%{$reset_color%}>\
 < at %{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}>\
 %{$fg[green]%}<status|modified|unknown><update>%{$reset_color%}<
 patches: <patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[white]%})|post_unapplied(%{$reset_color%})>>" 2>/dev/null
