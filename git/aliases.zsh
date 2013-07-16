c84d39708e189f9e969c7ff0ec0f684578131cb7
tadamak <tadamak@gmail.com>
2013-07-17 01:01:37 +0900

    rm hub alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias g=$hub_path
-else
-  alias g='git'
-fi
+alias g='git'
 
 # The rest of my fun git aliases
