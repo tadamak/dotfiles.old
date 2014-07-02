3fafec3cf067f742d39991dc81f70b05418c30fa
tadamak <tadamak@gmail.com>
2014-07-02 11:59:30 +0900

    add

---
+++ tmux/alias.zsh
@@ -0,0 +1,3 @@
+alias ta='tmux attach -t'
+alias ts='tmux new-session -s'
+alias tl='tmux list-sessions'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,8 +9,13 @@ alias pd='popd'
 alias hi='history'
 alias ...='cd ../..'
 alias _='sudo '
-
+alias src='cd ~/Dropbox/code'
 
 alias mosh='LC_CTYPE=$(echo $LANG) mosh'
 
-alias peco='peco --rcfile $HOME/.peco.json'
+a() { git add . $1 --all; git status --short }
+m() { git commit -m "$*" }
+u() { cd ./$(git rev-parse --show-cdup)/$1 }
+p() { peco | while read LINE; do $@ $LINE; done }
+
+alias q='ghq list -p | p cd'
