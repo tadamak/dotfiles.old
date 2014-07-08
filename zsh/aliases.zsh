33b403591d8db8a0a5e35d300ee18c6c75f6048c
tadamak <tadamak@gmail.com>
2014-07-08 13:12:45 +0900

    add

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,13 +9,12 @@ alias pd='popd'
 alias hi='history'
 alias ...='cd ../..'
 alias _='sudo '
-alias src='cd ~/Dropbox/code'
-
 alias mosh='LC_CTYPE=$(echo $LANG) mosh'
 
 a() { git add . $1 --all; git status --short }
 m() { git commit -m "$*" }
 u() { cd ./$(git rev-parse --show-cdup)/$1 }
 p() { peco | while read LINE; do $@ $LINE; done }
+src() { cd $HOME/Dropbox/code/$1 }
 
 alias q='ghq list -p | p cd'
